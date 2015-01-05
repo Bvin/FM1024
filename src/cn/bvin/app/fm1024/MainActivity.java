dcb88116bcb25be2af265c27112804b7c71f1141
bvin <bvin@live.cn>
2015-01-05 22:38:16 +0800

    添加对task返回播放地址进行播放测试

--- src/cn/bvin/app/fm1024/MainActivity.java
+++ src/cn/bvin/app/fm1024/MainActivity.java
@@ -1,6 +1,7 @@
 package cn.bvin.app.fm1024;
 
 import cn.bvin.app.fm1024.config.Urls;
+import cn.bvin.app.fm1024.play.SystemPlayer;
 import cn.bvin.app.fm1024.task.QTCatecoryTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -20,18 +21,28 @@ public class MainActivity extends FragmentActivity implements LoaderCallbacks<St
 
 	@Override
 	public Loader<String> onCreateLoader(int arg0, Bundle arg1) {
-		String url = Urls.getChannel(74044,1, 10);
+		String url = Urls.getChannel(74044,1, 1);
 		((TextView)findViewById(R.id.tv)).setText("请求"+url+"...");
 		return new QTCatecoryTask(getApplicationContext(), url);
 	}
 
 	@Override
 	public void onLoadFinished(Loader<String> arg0, String arg1) {
+		SystemPlayer.getInstance().setSource(arg1);
 		((TextView)findViewById(R.id.tv)).setText(arg1);
+		SystemPlayer.getInstance().play();
 	}
 
 	@Override
 	public void onLoaderReset(Loader<String> arg0) {
 		
 	}
+
+
+	@Override
+	protected void onDestroy() {
+		SystemPlayer.getInstance().stop();
+		super.onDestroy();
+	}
+	
 }
